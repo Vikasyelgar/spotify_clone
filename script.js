309f74a19cb077d6dafd5730fcf293ab77585d06
Vikasyelgar <138794556+Vikasyelgar@users.noreply.github.com>
2024-09-20 11:58:29 +0530

    spotify clone project

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector('.signup-button button').addEventListener('click', function() {
+        alert('Welcome to Sign UP Page !!!!');
+    });
+
+    function redirectToLogin() {
+        alert("Redirecting to login page...");
+    }
